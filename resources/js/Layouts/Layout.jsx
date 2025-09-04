f21c7c49338e699402cae6405c06f9eba600c98b
ibrahimdolas <33544751+ibrahimdolas@users.noreply.github.com>
2025-09-04 09:30:53 +0300

    added favicons and cookie consent

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -9,6 +9,7 @@ import BackToTop from "@/Components/BackToTop.jsx";
 import useGsapWow from "@/hooks/useGsapWow.js";
 import useParallax from "@/hooks/useParallax.js";
 import useTitleAnimation from "@/hooks/useTitleAnimation.js";
+import {CookieConsent} from "react-cookie-consent";
 
 
 
@@ -47,6 +48,22 @@ const titleTranslation = {
 }
 
 
+const cookieTexts = {
+	button: {
+		en: 'Understood!',
+		tr: 'Anladım!',
+		it: 'Capito!',
+		ro: 'Am capit!'
+	},
+	text: {
+		en: 'We use tools such as cookies to collect data about our website and services and how visitors interact with our website and services.',
+		tr: 'Sitemizde temel hizmetler ve işlevleri sağlamak ve ziyaretçilerin sitemiz ve hizmetlerimizle nasıl etkileşimde bulunduğuna dair verileri toplamak için tanımlama dosyaları gibi araçlar (Örneğin; çerezler gibi) kullanmaktayız.',
+		it: 'Utilizziamo strumenti come i cookie per raccogliere dati sul nostro sito web e sui nostri servizi e su come i visitatori interagiscono con essi.',
+		ro: 'Folosim instrumente precum cookie-urile pentru a colecta date despre site-ul și serviciile noastre și despre modul în care vizitatorii interacționează cu site-ul și serviciile noastre.',
+	}
+}
+
+
 
 
 const Layout = ({title, children}) => {
@@ -74,6 +91,29 @@ const Layout = ({title, children}) => {
 			<Footer/>
 			
 			<BackToTop/>
+			
+			
+			<CookieConsent
+				buttonText={cookieTexts.button[locale] ?? cookieTexts.button.en}
+				cookieName="mmm_cookie_consent_cookie"
+				debug={false}
+				style={{
+					background: "rgb(91, 90, 123)",
+					color: "#fff",
+					fontSize: "14px",
+					fontFamily: "sofia-pro, sans-serif",
+					fontWeight: "500",
+					textAlign: "center",
+					padding: "5px 50px",
+					position: "fixed",
+					bottom: "0",
+					left: "0",
+					right: "0",
+				}}
+			>
+				{cookieTexts.text[locale] ?? cookieTexts.text.en}
+			</CookieConsent>
+			
 			
 		</>
 	);
